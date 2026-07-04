ab7bb271b863e69a899caceff50a02f462c091d6
Caylusis2 <caylusis2@yahoo.com>
2026-07-04 07:34:02 +0000

    week1progress

--- L01_redCircle.js
+++ L01_redCircle.js
@@ -5,5 +5,10 @@ function setup(){
 
 function draw(){
     fill(255, 0, 0);
-    ellipse(300,200,200,200);
+    ellipse(300,200,50,50);
+        fill(0,0,255);
+    ellipse(300,250,50,50);
+        fill(0,250,0);
+    ellipse(300,300,50,50);
+
 }
