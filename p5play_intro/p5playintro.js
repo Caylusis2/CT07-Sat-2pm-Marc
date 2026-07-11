f74ebc47eace5fd95c3865a99cef0210e95c70c5
Caylusis2 <caylusis2@yahoo.com>
2026-07-11 06:57:11 +0000

    Jul 11, 2026, 6:57 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -2,8 +2,8 @@
 // let box;
 function setup() {
 //   // Set up the canvas
-//   new Canvas(400, 600);
-//   background(250); //background color
+  new Canvas(400, 600);
+  background(250); //background color
 
 //   // Basic shape testing
 //   // write your codes here
