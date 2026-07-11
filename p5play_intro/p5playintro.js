25acaa7f8f43e26abf15e541917d76152a6cb86f
Caylusis2 <caylusis2@yahoo.com>
2026-07-11 06:56:44 +0000

    Jul 11, 2026, 6:56 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -1,30 +1,30 @@
 let ball;
 let box;
 function setup() {
-  // Set up the canvas
-  new Canvas(400, 600);
-  background(250); //background color
+//   // Set up the canvas
+//   new Canvas(400, 600);
+//   background(250); //background color
 
-  // Basic shape testing
-  // write your codes here
- fill("skyblue");
-  // End Basic shape testing
-   circle(35,35,60)
-   rect(60,55,100,200)
-   // Create a bouncing ball sprite
-   // write your codes here
-  ball = new Sprite();
-  ball.x = 360;
-  ball.y = 35;
-  ball.diameter = 60;
-  ball.color = 'red';
+//   // Basic shape testing
+//   // write your codes here
+//  fill("skyblue");
+//   // End Basic shape testing
+//    circle(35,35,60)
+//    rect(60,55,100,200)
+//    // Create a bouncing ball sprite
+//    // write your codes here
+//   ball = new Sprite();
+//   ball.x = 360;
+//   ball.y = 35;
+//   ball.diameter = 60;
+//   ball.color = 'red';
 
-  box = new Sprite();
-  box.x = 100;
-  box.y = 100
-  box.w = 50;
-  box.h = 75;
-  box.color = 'skyblue';
+//   box = new Sprite();
+//   box.x = 100;
+//   box.y = 100
+//   box.w = 50;
+//   box.h = 75;
+//   box.color = 'skyblue';
 
  
   
