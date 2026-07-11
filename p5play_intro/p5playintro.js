d922c2112531ee5b9327afc9a0c0674dadaecffd
Caylusis2 <caylusis2@yahoo.com>
2026-07-11 06:28:54 +0000

    Jul 11, 2026, 6:28 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -7,7 +7,7 @@ function setup() {
   // write your codes here
  ball = new Sprite();
   // End Basic shape testing
-   circle(50,50,100)
+   circle(50,50,70)
    // Create a bouncing ball sprite
    // write your codes here
 
