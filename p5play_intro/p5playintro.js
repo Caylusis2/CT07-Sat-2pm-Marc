826ff6852b8e2d2d7a41a811c2afc927f20c71c2
Caylusis2 <caylusis2@yahoo.com>
2026-07-11 07:08:51 +0000

    Jul 11, 2026, 7:08 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -30,8 +30,8 @@ function setup() {
 //  ellipse(200,300,50,100);
 //  ellipse(200,300,50,30);
 
-circle(200,300,200);
-circle();
+circle(200,300,2300);
+// circle();
 // circle();
 // circle();
 // circle();
