952d38db5f2339319a9b9c3115381db6f1bc79e8
Caylusis2 <caylusis2@yahoo.com>
2026-07-11 06:35:49 +0000

    Jul 11, 2026, 6:35 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -9,7 +9,7 @@ function setup() {
  fill("skyblue");
   // End Basic shape testing
    circle(35,35,60)
-   rect(59,55,50,100)
+   rect(59,55,100,200)
    // Create a bouncing ball sprite
    // write your codes here
 
