d47d4f1d2099546c47959024f28b59d2d0d3ab66
Caylusis2 <caylusis2@yahoo.com>
2026-07-11 06:50:57 +0000

    Jul 11, 2026, 6:50 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -38,4 +38,16 @@ function draw() {
   textSize(16);
   text("Ball: (" + int(ball.x) + ", " + int(ball.y) + ")", 10, 20);
   text("Mouse: (" + mouseX + ", " + mouseY + ")", 10, 40);
+
+  if (ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
+    ball.vel.x *= -1;
+}
+
+  if (ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2) {
+    ball.vel.y *= -1;
+}
+
+box.x 
+
+
 }
