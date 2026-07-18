017ee864111fdd07a174186077c163b50db7e054
Caylusis2 <caylusis2@yahoo.com>
2026-07-18 07:02:36 +0000

    Jul 18, 2026, 7:02 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -5,13 +5,20 @@ background(220);
 textsize(16);
   // --- Exercise: Area of Triangle ---
   // write your codes here
-area = 0.5 * base * height
-console.log("The area of the triangle is :",area);
-text("The area of the triangle is :" + area,300,200);
+// area = 0.5 * base * height
+// console.log("The area of the triangle is :",area);
+// text("The area of the triangle is :" + area,300,200);
 
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
-
+  // for i in range(5);
+  // for(let i=0; i<5 ;i++){ }
+  // let sum =0;
+  // for(let i=0;i<=20;++i){
+  //   if(i%2==0){
+  //     sum += i;
+  //   }
+  // }
   // --- Exercise: Age category classification ---
   // write your codes here
 
