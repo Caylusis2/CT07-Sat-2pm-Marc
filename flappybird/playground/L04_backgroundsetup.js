84660f388eb56210c0d806903166b12ab8563e80
Caylusis2 <caylusis2@yahoo.com>
2026-07-25 07:09:56 +0000

    Jul 25, 2026, 7:09 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -2,3 +2,8 @@ let bird, floor;
 
 let midflapping, bg, base;
 
+function preload(){
+    midflapping = loadImage('redbird-midflap.png');
+    bg = loadImage('background-night.png');
+    base  = loadImage('base.png');
+}
