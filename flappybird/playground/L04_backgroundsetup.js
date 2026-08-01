b2c7df0ced913b993364d2a2b762af070fca3261
Caylusis2 <caylusis2@yahoo.com>
2026-08-01 06:19:59 +0000

    Aug 1, 2026, 6:19 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -36,7 +36,7 @@ function setup(){
 function draw(){
     image(bg,0,0,width , height);
 
-    if (KeyboardEvent.presses('space')){
+    if (KeyboarkbdEvent.presses('space')){
         bird.vel.y = -45;
         bird.sleeping = false;
     }
