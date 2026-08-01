158231d13746484422104f254aabb86a639a6963
Caylusis2 <caylusis2@yahoo.com>
2026-08-01 07:11:05 +0000

    Aug 1, 2026, 7:11 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -4,7 +4,7 @@ let pipeGroup;
 let midflaping, bg, base;
 let flapUpImg,flapDownImg
 let pipe;
-let toppepe,bottomPipe;
+let toppipe,bottomPipe;
 
 function preload(){
     midflaping = loadImage('assets/redbird-midflap.png');
