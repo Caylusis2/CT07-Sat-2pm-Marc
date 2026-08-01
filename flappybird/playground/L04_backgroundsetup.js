c1d12c809b02e311f8f266293d1615728428a136
Caylusis2 <caylusis2@yahoo.com>
2026-08-01 06:14:28 +0000

    Aug 1, 2026, 6:14 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -30,9 +30,11 @@ function setup(){
     floor.height = 125;
     floor.collider = "static";
     floor.img = base;
-    
+
 }
 
 function draw(){
     image(bg,0,0,width , height);
+
+    if (KeyboardEvent.presses('space'))
 }
