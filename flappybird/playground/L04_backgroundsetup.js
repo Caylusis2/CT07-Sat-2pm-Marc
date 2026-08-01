de4763215c33d88eac3c630a5560e00722beef49
Caylusis2 <caylusis2@yahoo.com>
2026-08-01 07:11:50 +0000

    Aug 1, 2026, 7:11 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -83,5 +83,5 @@ function spawnPipePair(){
     pipeGroup.add(bottomPipe);
     pipeGroup.layer = 0;
 
-    topPipe = new Sprite(400, midY - gap / 2 -200, 52, 320)
+    topPipe = new Sprite(400, midY - gap / 2 -200, 52, 320, 'static');
 }
