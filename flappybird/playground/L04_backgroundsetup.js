9612bea064c6ffc1469511e230cdc176de423880
Caylusis2 <caylusis2@yahoo.com>
2026-08-08 06:52:16 +0000

    Aug 8, 2026, 6:52 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -87,6 +87,17 @@ function draw(){
     }
 
     drawScore(width/2, 20, score, 24, 36)
+
+    for (let pipe of pipeGroup) {
+        let pipeRightEdge = pipe.x + pipe.w / 2;
+        let birdLeftEdge = bird.x - bird.w / 2;
+
+        if (pipe.passed == flase && pipeRightEdge < birdLeftEdge){
+            pipe.passed = true;
+            score++;
+        }
+    }   
+
 }
 
 function spawnPipePair(){
