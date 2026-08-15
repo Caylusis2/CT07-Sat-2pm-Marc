dac2ff5f63ede0160626d5281ddb5cd2da467ba4
Caylusis2 <caylusis2@yahoo.com>
2026-08-15 06:14:12 +0000

    Aug 15, 2026, 6:14 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -55,7 +55,7 @@ function setup(){
     floor.height = 125;
     floor.collider = 'static';
     floor.img = base;
-    startScreenLabel = new Sprite(width)
+    startScreenLabel = new Sprite(width/2, height)
 
 }
 
