7113dfbf48e1c3fbdce6a91ac3d7f4f7be0465e5
Caylusis2 <caylusis2@yahoo.com>
2026-08-15 06:34:53 +0000

    Aug 15, 2026, 6:34 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -140,7 +140,7 @@ function spawnPipePair(){
     let gap = 50;
     let midY = random(250, height - 250);
 
-    bottomPipe = new Sprite(bird,x + 400, midY + gap / 2 + 200, 52, 320, 'static');
+    bottomPipe = new Sprite(bird.x + 400, midY + gap / 2 + 200, 52, 320, 'static');
     bottomPipe.img = pipe;
 
     pipeGroup.add(bottomPipe);
