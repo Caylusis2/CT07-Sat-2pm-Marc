710319a718e5778762acacd0153c1d5b63f40543
Caylusis2 <caylusis2@yahoo.com>
2026-09-19 06:26:58 +0000

    Sep 19, 2026, 6:26 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -47,10 +47,10 @@ function spawnFruit(){
     let fruitData = random(fruitTypes);
     let randomX = random(300, 500);
     let fruit = new fruitGroup.Sprite(randomX, height+20, 40);
-    fruit.image = fruitData.whole;
-    fruit.type = fruitData;
-    fruit.vel.y = random(-10, -14);
-    fruit.vel.x = random(-2, 2);
+    fruit.image = fruitData.whole; 
+    fruit.type = fruitData; 
+    fruit.vel.y = random(-10, -14); 
+    fruit.vel.x = random(-2, 2); 
     fruit.friction = 0;
 }
 
